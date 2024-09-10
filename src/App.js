87877a5495561bbbf3f53fdfbfbf4d980219a80c
Attila Fekete <feketeattila777@proton.me>
2024-09-10 20:49:10 +0200

    Image double generation and basic scoring

--- src/App.js
+++ src/App.js
@@ -10,21 +10,27 @@ const pictures = [
   { id: 7, route: "pictures/7_400.jpg", name: "window" },
   { id: 8, route: "pictures/8_400.jpg", name: "street" },
 ];
-export default function App() {
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i >= 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
+  return array;
+}
+let doublePictures = pictures.flatMap((el, i) => [{ ...el }, { ...el, id: i + 9 }]);
+doublePictures = shuffleArray(doublePictures);
+console.log(doublePictures);
+
+export default function App() {
+  const [score, setScore] = useState(0);
+  const [picName, setPicName] = useState([]);
 
   function createRandomPictureArray() {
-    let arr = pictures.map((pic) => <Card propsObj={pic} key={pic.id} />);
-    arr = [...arr, pictures.map((pic) => <Card propsObj={pic} key={pic.id + 0.1} />)];
-    // arr = [...arr, pictures.map((pic) => <Card propsObj={pic} key={pic.id + 0.2} />)];
-    // arr = [...arr, pictures.map((pic) => <Card propsObj={pic} key={pic.id + 0.3} />)];
-    shuffleArray(arr);
-    return arr;
+    const cardComponents = doublePictures.map((pic) => (
+      <Card propsObj={pic} setScore={setScore} picName={picName} setPicName={setPicName} key={pic.id} />
+    ));
+    return cardComponents;
   }
 
   return (
@@ -33,44 +39,39 @@ export default function App() {
         <h1>Memory Game</h1>
       </header>
       <div className="score">
-        Your Current Score Is:<p className="pairs">X</p>
+        Your Current Score Is:<p className="pairs">{score}</p>
       </div>
       <div className="playfield">{createRandomPictureArray()}</div>
     </div>
   );
 }
 
-function Card({ propsObj }) {
-  const [clicked, setClicked] = useState(0);
-  const [hovered, setHovered] = useState(false);
+function Card({ propsObj, setScore, picName, setPicName }) {
+  const [clicked, setClicked] = useState([0]);
 
-  function handleMouseEnter() {
-    setHovered(true);
-  }
-  function handleMouseLeave() {
-    setHovered(false);
-  }
+  function handleClick(propsObj) {
+    setClicked(propsObj.id);
 
-  function handleClick(id) {
-    setClicked(id);
+    if (propsObj.name === picName[picName.length - 1]) {
+      setScore((s) => s + 1);
+    } else {
+      setPicName((el) => [...el, propsObj.name]);
+    }
   }
 
   return (
-    <div onClick={() => handleClick(propsObj.id)}>
+    <div onClick={() => handleClick(propsObj)}>
       {clicked === propsObj.id ? (
         <img src={propsObj.route} alt={propsObj.name}></img>
       ) : (
-        <img class="hover-mod" src="pictures/card_back_400.png" alt="card-back"></img>
+        <img className={"hover-mod"} src="pictures/card_back_400.png" alt="card-back"></img>
       )}
     </div>
   );
 }
 
 {
-  // pic and background size have to be equal
-  // have to change how the pics rendered, should use some flexbox or smthg
-  // double the pictures, make it fix
-  // make the card turn back if clicked again
+  // make the card turn back if not a pair
   // clicked can be an array to hold both data?
   // score if correct and do not turn them back or remove them completely?
   // turn them back if not correct
